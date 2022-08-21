98d5bd56fe12ae9f0d86c35f50b7830762558a50
Kim-Ji-Seop <76149358+Kim-Ji-Seop@users.noreply.github.com>
2022-08-21 14:08:31 +0900

    mumin

---
+++ Desktop/api-server-spring-boot/src/main/java/com/example/demo/utils/SHA256.java
@@ -0,0 +1,77 @@
+<<<<<<< HEAD
+package com.example.demo.utils;
+
+import java.security.MessageDigest;
+
+public class SHA256 {
+    public SHA256() {
+    }
+
+    public static String encrypt(String planText) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            md.update(planText.getBytes());
+            byte[] byteData = md.digest();
+            StringBuffer sb = new StringBuffer();
+
+            for(int i = 0; i < byteData.length; ++i) {
+                sb.append(Integer.toString((byteData[i] & 255) + 256, 16).substring(1));
+            }
+
+            StringBuffer hexString = new StringBuffer();
+
+            for(int i = 0; i < byteData.length; ++i) {
+                String hex = Integer.toHexString(255 & byteData[i]);
+                if (hex.length() == 1) {
+                    hexString.append('0');
+                }
+
+                hexString.append(hex);
+            }
+
+            return hexString.toString();
+        } catch (Exception var7) {
+            var7.printStackTrace();
+            throw new RuntimeException();
+        }
+    }
+}
+=======
+package com.example.demo.utils;
+
+import java.security.MessageDigest;
+
+public class SHA256 {
+    public SHA256() {
+    }
+
+    public static String encrypt(String planText) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            md.update(planText.getBytes());
+            byte[] byteData = md.digest();
+            StringBuffer sb = new StringBuffer();
+
+            for(int i = 0; i < byteData.length; ++i) {
+                sb.append(Integer.toString((byteData[i] & 255) + 256, 16).substring(1));
+            }
+
+            StringBuffer hexString = new StringBuffer();
+
+            for(int i = 0; i < byteData.length; ++i) {
+                String hex = Integer.toHexString(255 & byteData[i]);
+                if (hex.length() == 1) {
+                    hexString.append('0');
+                }
+
+                hexString.append(hex);
+            }
+
+            return hexString.toString();
+        } catch (Exception var7) {
+            var7.printStackTrace();
+            throw new RuntimeException();
+        }
+    }
+}
+>>>>>>> cd4e62ec842940fe34348125108739265881e4d4
