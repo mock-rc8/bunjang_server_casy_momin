bf7dc88d14e05cccd29c47c2fd47ad1fd8ecf498
Kim-Ji-Seop <76149358+Kim-Ji-Seop@users.noreply.github.com>
2022-08-21 19:40:55 +0900

    server address설정

--- Desktop/webService/src/test/java/com/example/demo/DemoApplicationTests.java
+++ Desktop/webService/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -3,12 +3,12 @@ package com.example.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class DemoApplicationTests {
-
-    @Test
-    void contextLoads() {
-        System.out.println("SuccessTest");
-    }
-
-}
+//@SpringBootTest
+//class DemoApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//
+//    }
+//
+//}
