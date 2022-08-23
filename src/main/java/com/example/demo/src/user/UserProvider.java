580c88f9b739ee8a19d0d4c4e14bd0300d656de0
Kim-Ji-Seop <76149358+Kim-Ji-Seop@users.noreply.github.com>
2022-08-23 12:15:04 +0900

    커밋 테스트1

--- src/main/java/com/example/demo/src/user/UserProvider.java
+++ src/main/java/com/example/demo/src/user/UserProvider.java
@@ -23,7 +23,7 @@ import static com.example.demo.config.BaseResponseStatus.*;
  */
 public class UserProvider {
 
-
+    //git commit test1
     // *********************** 동작에 있어 필요한 요소들을 불러옵니다. *************************
     private final UserDao userDao;
     private final JwtService jwtService; // JWT부분은 7주차에 다루므로 모르셔도 됩니다!
