e5be2ee930fe7549255abb15bd2085d23b4b9d8d
이정민 <85864699+JjungminLee@users.noreply.github.com>
2022-08-24 22:12:16 +0900

    Update BaseResponseStatus.java

--- src/main/java/com/example/demo/config/BaseResponseStatus.java
+++ src/main/java/com/example/demo/config/BaseResponseStatus.java
@@ -22,13 +22,27 @@ public enum BaseResponseStatus {
     INVALID_JWT(false, 2002, "유효하지 않은 JWT입니다."),
     INVALID_USER_JWT(false,2003,"권한이 없는 유저의 접근입니다."),
 
+    
+
     // users
     USERS_EMPTY_USER_ID(false, 2010, "유저 아이디 값을 확인해주세요."),
-
     // [POST] /users
-    POST_USERS_EMPTY_EMAIL(false, 2015, "이메일을 입력해주세요."),
-    POST_USERS_INVALID_EMAIL(false, 2016, "이메일 형식을 확인해주세요."),
+    POST_USERS_EMPTY_PHONE_NUMBER(false, 2004, "핸드폰 번호를 입력해주세요."),
+    POST_USERS_DEFAULT_RANGE_PHONE_NUMBER(false, 2005, "핸드폰 번호를 4자리 이상 입력해주세요."),
+    POST_USERS_INVALID_PHONE_NUMBER(false, 2006, "핸드폰 번호를 다시 확인해주세요."),
+    POST_USERS_EMPTY_RESIDENT_NUMBER(false, 2007, "주민등록번호를 입력해주세요."),
+    POST_USERS_INVALID_RESIDENT_NUMBER(false, 2008, "주민등록번호를 다시 확인해주세요."),
+    POST_USERS_INVALID_USER_NAME(false, 2009, "이름을 다시 확인해주세요."),
+    POST_USERS_INVALID_PASSWORD(false, 2011, "비밀번호를 다시 확인해주세요."),
+    POST_USERS_INVALID_STORE_NAME(false, 2012, "상점명을 다시 확인해주세요."),
+
+    //회원가입
     POST_USERS_EXISTS_STORE_NAME(false,2017,"중복된 상점 이름입니다."),
+    //배송지 추가, 수정
+    POST_USERS_EXISTS_SHIPPING_INFO(false,2018,"같은 배송지가 존재합니다."),
+    //로그인
+    POST_USERS_INACTIVE_USER(false,2019,"휴면처리된 회원입니다."),
+    POST_USERS_DELETE_USER(false,2020,"탈퇴 후 7일간은 다시 가입하실 수 없습니다."),
 
 
 
@@ -53,12 +67,21 @@ public enum BaseResponseStatus {
     //[PATCH] /users/{userIdx}
     MODIFY_FAIL_USERNAME(false,4014,"유저네임 수정 실패"),
 
-
     PASSWORD_ENCRYPTION_ERROR(false, 4011, "비밀번호 암호화에 실패하였습니다."),
-    PASSWORD_DECRYPTION_ERROR(false, 4012, "비밀번호 복호화에 실패하였습니다.");
+    PASSWORD_DECRYPTION_ERROR(false, 4012, "비밀번호 복호화에 실패하였습니다."),
 
 
     // 5000 : 필요시 만들어서 쓰세요
+    
+    // 상점후기 -> 글자수 제한
+    POST_REVIEWS_TEXT_LENGTH(false,5000,"글자수가 10자 미만입니다."),
+
+    // 상점후기 -> 이미 리뷰 작성한 경우 판단
+    POST_REVIEWS_EXISTS_PRODUCTS_REVIEW(false,5001,"이미 리뷰를 작성하셨습니다."),
+
+    // 이미 팔로우 한 상점이라면
+    POST_STORES_EXISTS_FOLLOWING(false,5002,"이미 팔로우하고 있는 상점입니다.");
+
     // 6000 : 필요시 만들어서 쓰세요
 
 
