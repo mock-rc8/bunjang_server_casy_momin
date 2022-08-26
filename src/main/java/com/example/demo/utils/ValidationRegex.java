16bb0a6e474c301c60766dce6300eedaf55ea822
김지섭 <76149358+Kim-Ji-Seop@users.noreply.github.com>
2022-08-26 11:56:06 +0900

    상점명 최대10자 수정

--- src/main/java/com/example/demo/utils/ValidationRegex.java
+++ src/main/java/com/example/demo/utils/ValidationRegex.java
@@ -35,7 +35,7 @@ public class ValidationRegex {
         return matcher.find();
     }
     public static boolean isRegexStoreName(String target) { // 상점명 정규식 - 2자 이상 16자 이하, 영어 또는 숫자 또는 한글로 구성
-        String regex = "^(?=.*[a-z0-9가-힣])[a-z0-9가-힣]{2,16}$"; // * 특이사항 : 한글 초성 및 모음은 허가하지 않는다.
+        String regex = "^(?=.*[a-z0-9가-힣])[a-z0-9가-힣]{2,10}$"; // * 특이사항 : 한글 초성 및 모음은 허가하지 않는다.
         Pattern pattern = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
         Matcher matcher = pattern.matcher(target);
         return matcher.find();
