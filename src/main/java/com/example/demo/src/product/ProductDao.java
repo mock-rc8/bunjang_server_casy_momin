6d7a8c04cc72e440ed969f3d4b3d89e494e30594
김지섭 <76149358+Kim-Ji-Seop@users.noreply.github.com>
2022-08-30 22:44:04 +0900

    2022.08.30 - status값 추가
    캐시님 대신 커밋했습니다 죄송합니다!

--- src/main/java/com/example/demo/src/product/ProductDao.java
+++ src/main/java/com/example/demo/src/product/ProductDao.java
@@ -193,7 +193,7 @@ public class ProductDao {
 
 
 
-                "(select count(productsID) from Heart where Heart.productsID=Products.ID) as heartCount," +
+                "(select count(productsID) from Heart where Heart.productsID=Products.ID and status='A') as heartCount," +
                 "(select exists(select userID from Heart where userID=? AND Heart.productsID=Products.ID)) as userHeart," +
                 "pay " +
                 "from Products " +
@@ -270,7 +270,7 @@ public class ProductDao {
                 "                                end as created, " +
 
                 "                (select count(productsID) from Views where Views.productsID=Products.ID) as views," +
-                "                (select count(Heart.productsID) from Heart where Heart.productsID=Products.ID) as heartCount, " +
+                "                (select count(Heart.productsID) from Heart where Heart.productsID=Products.ID and status='A') as heartCount, " +
                 "                productStatus,quantity,shippingFee,exchange,Products.productExplain " +
 
                 "                from Products " +
